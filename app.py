4a0f94f46eebcf403b2ba67ec2144bf9ffd8840f
ponkaliraj26 <ponkali.raj@greenvalleyschools.in>
2025-06-15 13:03:11 +0000

    joblist.html added

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+JOBS = [{
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Rs. 10,00,000"
+}, {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "Delhi, India",
+    "salary": "Rs. 15,00,000"
+}, {
+    "id": 3,
+    "title": "Frontend Engineer",
+    "location": "Remote",
+    "salary": "Rs. 12,00,000"
+}, {
+    "id": 4,
+    "title": "Backend Engineer",
+    "location": "San Francisco, USA",
+    "salary": "$120,000"
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
